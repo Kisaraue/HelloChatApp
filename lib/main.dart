7818a37e0d58a9b5e5fe1d1d8187f6d8b322ee4d
Kisara_Ekanayake <kisaraue5@gmail.com>
2022-01-24 13:32:48 -0800

    Seccond Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,28 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hello_chat/ui/splash/splash_screen.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:hello_chat/Backend/firebase/OnlineDatabaseManagement/cloud_data_management.dart';
-import 'package:hello_chat/FrontEnd/AuthUI/log_in.dart';
-import 'package:hello_chat/FrontEnd/MainScreens/main_screen.dart';
-import 'package:hello_chat/FrontEnd/NewUserEntry/new_user_entry.dart';
-import 'package:hello_chat/Global_Uses/foreground_receive_notificaion_management.dart';
-
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseFirestore.instance.settings =
+      const Settings(persistenceEnabled: true);
+  runApp(const MyApp());
+}
 
-  /// Initialize Notification Settings
-  await notificationInitialize();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  /// For Background Message Handling
-  FirebaseMessaging.onBackgroundMessage(backgroundMsgAction);
-
-  /// For Foreground Message Handling
-  FirebaseMessaging.onMessage.listen((messageEvent) {
-    print(
-        "Message Data is: ${messageEvent.notification!.title}     ${messageEvent.notification!.body}");
-
-    _receiveAndShowNotificationInitialization(
-        title: messageEvent.notification!.title.toString(),
-        body: messageEvent.notification!.body.toString());
-  });
-
-  runApp(
-    MaterialApp(
-      title: 'HelloChat',
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      home: await differentContextDecisionTake(),
-    ),
-  );
-}
-
-Future<Widget> differentContextDecisionTake() async {
-  if (FirebaseAuth.instance.currentUser == null) {
-    return LogInScreen();
-  } else {
-    final CloudStoreDataManagement _cloudStoreDataManagement =
-        CloudStoreDataManagement();
-
-    final bool _dataPresentResponse =
-        await _cloudStoreDataManagement.userRecordPresentOrNot(
-            email: FirebaseAuth.instance.currentUser!.email.toString());
-
-    return _dataPresentResponse ? MainScreen() : TakePrimaryUserData();
+      title: 'Hello Chat',
+      theme: ThemeData(
+       backgroundColor: Color(0xff070c44),
+      ),
+      home: const SplashScreen(),
+    );
   }
-}
-
-Future<void> notificationInitialize() async {
-  /// Subscribe to a topic
-  await FirebaseMessaging.instance.subscribeToTopic("Generation_YT");
-
-  /// Foreground Notification Options Enabled
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-}
-
-/// Receive And show Notification Customization
-void _receiveAndShowNotificationInitialization(
-    {required String title, required String body}) async {
-  final ForegroundNotificationManagement _fgNotifyManagement =
-      ForegroundNotificationManagement();
-
-  print("Notification Activated");
-
-  await _fgNotifyManagement.showNotification(title: title, body: body);
-}
-
-Future<void> backgroundMsgAction(RemoteMessage message) async {
-  await Firebase.initializeApp();
-
-  _receiveAndShowNotificationInitialization(
-      title: message.notification!.title.toString(),
-      body: message.notification!.body.toString());
 }
